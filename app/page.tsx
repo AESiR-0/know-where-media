b1ea3406b24895c7e45a9b74aad91c35d9af18b9
AESiR <cyborgkiller1008@gmail.com>
2025-02-19 03:20:20 +0530

    fixed: made it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -24,14 +24,14 @@ const Home: NextPage = () => {
       <div className="mt-20 text-center px-4 md:px-0">
         <div className="gap-4 flex flex-col items-center md:space-y-3">
           <div className="">
-            <h2 className="max-md:text-3xl md:text-5xl  font-spaceGrotesk font-semibold">
+            <h2 className="max-md:text-2xl md:text-5xl  font-spaceGrotesk font-semibold">
               From Nowhere  to Knowhere
             </h2>
-            <h1 className="landig-css max-w-6xl font-oswald uppercase px-4 md:px-20 lg:px-40 max-md:text-4xl md:text-8xl ">
+            <h1 className="landig-css max-w-6xl max-md:max-w-8xl max-md:px-0 font-oswald uppercase px-4 md:px-20 lg:px-40 max-md:text-6xl md:text-8xl ">
               SHOW YOUR HIDDEN TALENT
             </h1>
           </div>
-          <p className="text-3xl landing-text max-w-3xl px-32  flex justify-center items-center ">
+          <p className="text-3xl max-md:max-w-5xl max-md:px-10 max-md:text-xl landing-text max-w-3xl px-32  flex justify-center items-center ">
             We're redefining  how talent and brands connect, thrive, and shine
           </p>
           <button className="bg-custom-yellow hover:bg-custom-yellow/90 transition-all duration-300 text-black px-4 md:px-6 py-2 md:py-3 rounded font-bold text-sm md:text-base">
